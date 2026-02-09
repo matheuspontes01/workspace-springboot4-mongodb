d3bb3adf05369b4a7eef893540a51a5466b5890a
matheuspontes01 <matheus.pontes@mail.uft.edu.br>
2026-02-09 10:22:16 -0300

    Projeto spring criado

---
+++ src/main/java/com/matheus/workspacemongodb/WorkspacemongodbApplication.java
@@ -0,0 +1,13 @@
+package com.matheus.workspacemongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkspacemongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkspacemongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matheus/workspacemongodb/WorkspacemongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheus.workspacemongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkspacemongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
