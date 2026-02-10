81a1dc38e7c99b8f69baa1b54ecf99744059200a
matheuspontes01 <matheus.pontes@mail.uft.edu.br>
2026-02-10 10:52:50 -0300

    Endpoint para retornar os posts do usuario e obtendo post por id

---
+++ src/main/java/com/matheus/workspacemongodb/services/PostService.java
@@ -0,0 +1,22 @@
+package com.matheus.workspacemongodb.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.matheus.workspacemongodb.domain.Post;
+import com.matheus.workspacemongodb.repository.PostRepository;
+import com.matheus.workspacemongodb.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+	}
+}
