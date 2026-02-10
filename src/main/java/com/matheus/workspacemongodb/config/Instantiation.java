d5184819b2415af7dc33e4ccb56edb5efb16eb92
matheuspontes01 <matheus.pontes@mail.uft.edu.br>
2026-02-10 10:43:27 -0300

    Projeção de dados do autor com DTO e referencia de posts do user

--- src/main/java/com/matheus/workspacemongodb/config/Instantiation.java
+++ src/main/java/com/matheus/workspacemongodb/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.matheus.workspacemongodb.domain.Post;
 import com.matheus.workspacemongodb.domain.User;
+import com.matheus.workspacemongodb.dto.AuthorDTO;
 import com.matheus.workspacemongodb.repository.PostRepository;
 import com.matheus.workspacemongodb.repository.UserRepository;
 
@@ -35,12 +36,15 @@ public class Instantiation implements CommandLineRunner{
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar pra São Paulo. Abraços!", maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
-		
-		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar pra São Paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+		
 		postRepository.saveAll(Arrays.asList(post1, post2));
+		
+		maria.getPosts().addAll(Arrays.asList(post1, post2));
+		userRepository.save(maria);
 	}
 
 }
